712f8b0b553fa4fe96ca83c673ebc1fea58f2b0c
Aptem <maleev2002@yandex.ru>
2022-05-22 16:30:52 +0300

    Доработана система логирования

--- src/bot.py
+++ src/bot.py
@@ -1,11 +1,18 @@
 import discord
 import asyncio
 import logging
+import datetime
 
 from discord.ext import commands
+from config import Config
+
+# Initialize logger
+path = Config.get('log', 'path') + str(datetime.datetime.now().date()) + '.log'
+logging.basicConfig(filename=path,format='[%(asctime)s] [%(levelname)s]: %(message)s', datefmt='%y-%m-%d %H:%M:%S', encoding='utf-8')
+logging.warning('Start logging')
 
 from libs import Database
-from config import Config
+from modules import UtilityCog
 
 class CustomBot(commands.Bot):
     def __init__(self):
@@ -40,48 +47,11 @@ class CustomHelpCommand(commands.HelpCommand):
         print("command_not_found")
         return await super().command_callback(ctx, command=command)
 
-# TODO: Перенести в отдельный файл
-class UtilityCog(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-        self.config = Config()
-    
-    @commands.command()
-    @commands.has_permissions(administrator = True)
-    async def ping(self, ctx):
-        msg = await ctx.send(f'pong in {round(self.bot.latency * 1000)} ms!')
-        await asyncio.sleep(5)
-        await msg.delete()
-
-    @ping.error
-    async def ping_error(self, ctx, error):
-        if (str(ctx.channel).startswith("Direct Message with")):
-            return
-        msg = await ctx.send(embed = discord.Embed(description = f'{error}', color = int(Config.get('embed', 'accent_color'), 16)))
-        await asyncio.sleep(5)
-        await msg.delete()
-    
-    @commands.Cog.listener()
-    async def on_member_join(self, member):
-        channel = member.guild.system_channel
-        if channel is not None:
-            await channel.send(f'Welcome {member.mention}.')
-
-logging.basicConfig(filename='logs/latest.log',format='%(asctime)s: %(message)s', datefmt='[%y-%m-%d %H:%M:%S]')
-logging.warning('Start logging')
-
-# TODO: Перенести в отдельный файл
-def setupUtilityCog(CustomBot):
+# Подключение модули
+def setupCogs(CustomBot):
     bot.add_cog(UtilityCog(bot))
 
 bot = CustomBot()
-setupUtilityCog(bot)
-
-# @bot.event
-# async def on_command_error(ctx, error):
-#     if isinstance(error, commands.CommandNotFound):
-#         msg = await ctx.send(embed = discord.Embed(description = f'Command not found', color = embed_setting['accent_color']))
-#         await asyncio.sleep(3)
-#         await msg.delete()
+setupCogs(bot)
 
 bot.run(Config.get('bot', 'token'))
--- src/config.py
+++ src/config.py
@@ -1,5 +1,8 @@
 import json
+#import logging
 import os.path
+
+#logger = logging.getLogger(__name__)
 
 # Singleton 
 class Config():
@@ -19,21 +22,15 @@ class Config():
 
     @classmethod
     def get(cls, settingsType, key):
-        # if cls.__instance is None: 
-        #     cls.__new__(cls)
         return cls.__json_string[settingsType][key]
 
     @classmethod
     def set(cls, settingsType, key):
-        # if cls.__instance is None: 
-        #     cls.__new__(cls)
         cls.__json_string['server']['auto_role'] = '4e'
         cls.update()
 
     @classmethod
     def update(cls):
-        # if cls.__instance is None: 
-        #     cls.__new__(cls)
         path = os.getcwd() + '\data\settings.json'
         with open(path, 'w', encoding='utf-8') as settings:
             json.dump(cls.__json_string, settings, ensure_ascii=False, indent=4)
