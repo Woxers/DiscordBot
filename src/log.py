8a3819ad76abccc2fa424511f54bbc6906cd1c88
Aptem <maleev2002@yandex.ru>
2022-05-28 18:10:52 +0300

    Исправления

--- src/log.py
+++ src/log.py
@@ -4,9 +4,13 @@ import logging
 import datetime
 
 from config import Config
+from sys import platform
 
 # Initialize logger
-path = Config.get('log', 'path') + 'latest.log'
+if platform == "linux" or platform == "linux2":
+    path = '/home/DiscordBot/latest.log'
+else:
+    path = Config.get('log', 'path') + 'latest.log'
 
 logging.basicConfig(filename=path, filemode='w',format='[%(asctime)s] [%(levelname)s]: %(message)s', datefmt='%y-%m-%d %H:%M:%S', encoding='utf-8', level=20)
 logging.info('Start logging')
