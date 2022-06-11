95b0df05db53dc3538a8df0a3f92ea0c0ce64e6e
Woxerss <maleev2002@yandex.ru>
2022-06-11 19:55:23 +0300

    dev

--- src/bot.py
+++ src/bot.py
@@ -1,6 +1,8 @@
 import os
+from tokenize import String
 import discord
 import asyncio
+from os import listdir
 import logging
 import datetime
 
@@ -8,9 +10,6 @@ from discord.ext import commands
 from help import CustomHelpCommand
 from config import Config
 from sys import platform
-
-if platform == "linux" or platform == "linux2":
-    from modules import VerificationCog, WelcomeCog, OwnerCog, UtilityCog
 
 import log
 
@@ -32,18 +31,35 @@ class CustomBot(commands.Bot):
             await self.sendEmbed(ctx, 'Command not found', None, 3, 'error')
 
     def setupCogs(self):
-        if platform == "linux" or platform == "linux2":
-            self.add_cog(OwnerCog(self))
-            self.add_cog(VerificationCog(self))
-            self.add_cog(WelcomeCog(self))
-            self.add_cog(UtilityCog(self))
-        else:
-            for filename in os.listdir('./modules'):
-                if filename.endswith('.py') and not filename.startswith('__'):
-                    self.load_extension(f'modules.{filename[:-3]}')
-                    print(f'load: {filename[:-3]}')
-                else:
-                    print(f'Unable to load {filename[:-3]}')
+        for filename in listdir('/home/aptem/VScodeRep/DiscordBot/src/modules'):
+            if filename.endswith('.py') and not filename.startswith('__'):
+                self.load_extension(f'modules.{filename[:-3]}')
+                print(f'load: {filename[:-3]}')
+            else:
+                print(f'Unable to load {filename[:-3]}')
+
+    async def sendEmbedd(self, channel: discord.channel, title = None, description = None, duration: int = None, color = None, footer_text = None, footer_icon = None, author_name = None, author_icon = None, timestamp: bool = None):
+        embed = discord.Embed()
+        if (title != None):
+            embed.title = title
+        if (description != None):
+            embed.description = description
+        if (color != None):
+            embed.color = color
+        if (footer_text != None) or (footer_icon != None):
+            print(footer_text)
+            print(footer_icon)
+            embed.set_footer(text = footer_text, icon_url= footer_icon)
+        if (author_name != None or author_icon != None):
+            embed.set_author(name=author_name, icon_url=author_icon)
+        if (timestamp == True):
+            embed.timestamp = datetime.datetime.utcnow()
+        
+        msg = await channel.send(embed = embed)
+
+        if (duration != None):
+            await asyncio.sleep(duration)
+            await msg.delete()
 
     async def sendEmbed(self, ctx, title = None, description = None, duration = 0, color = None):
         embed = discord.Embed(title = f'{title}', color = Config.getColor(color))
