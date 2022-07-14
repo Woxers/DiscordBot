65d952c582a26bf2b0b860439687973103cb7e54
Woxerss <maleev2002@yandex.ru>
2022-07-14 17:57:11 +0300

    Изменение класса для работы с базой данных

--- src_2_0/database.py
+++ src_2_0/database.py
@@ -1,9 +1,8 @@
 import time
-import secrets
 import pymysql
+
 import hashlib
 import secrets
-
 
 from config import get_color, config
 from logger import log_info, log_error
@@ -51,20 +50,34 @@ class Database:
             log_error(f'The exception in execute_query occured, {err_string}')
             return -1
 
-    # Check if nickname already exists
-    # Return 'True' - if exists, else - 'False'
     @classmethod
     def authme_check_nickname(cls, nickname: str):
+        '''
+        Check if nickname already in use
+
+        Return values: 
+
+            Exist: 1
+
+            Not Exist: 0
+        '''
         result = cls.execute_query(f'SELECT CheckNickname("{nickname}")')
         if (result == -1):
             return 1
         else:
             return result[0][0]
 
-    # Register player on minecraft server
-    # Return 'True' if successfully registered, else - 'False'
     @classmethod
     def authme_register_player(cls, user_id: int, nickname: str, password: str):
+        '''
+        Register player on minecraft server
+
+        Return values: 
+
+            Registered: 1
+
+            Failed: 0
+        '''
         if (cls.authme_check_nickname(nickname)):
             return 0
         # authme SHA256 encryption
@@ -81,23 +94,41 @@ class Database:
         else:
             return 1
     
-    # Check if user already exists in database
-    # Return 'True' - if exists, else - 'False'
     @classmethod
-    def discord_ckeck_user(cls, user_id: int):
+    def ckeck_user(cls, user_id: int):
+        '''
+        Check if discord user already in database
+
+        Return values: 
+
+            Exist: 1
+
+            Not Exist: 0
+        '''
         result = cls.execute_query(f'SELECT CheckUser({user_id})')
         if (result == -1):
             return 1
         else:
             return result[0][0]
 
-    # Gets user by id from database
-    # Returns dict: id, inviter_id, status
     @classmethod
-    def discord_get_user_by_id(cls, user_id: int):
+    def get_user_by_id(cls, user_id: int):
+        '''
+        Get discord user info by id 
+
+        Return values: 
+
+            Success:
+
+                dict: {'id', 'inviter_id', 'status'}
+
+            User not exist:
+
+                None
+        '''
         result = cls.execute_query(f'SELECT * FROM discord WHERE id = {user_id}')
         if (result == -1):
-            return 0
+            return None
         else:
             user = dict()
             user['id'] = result[0][0]
@@ -105,24 +136,69 @@ class Database:
             user['status'] = result[0][2]
             return user
 
-    # Gets user_id by nickname from database
-    # Returns user_id if success, else - int(0)
     @classmethod
-    def get_user_id_by_nickname(cls, nickname: str):
-        result = cls.execute_query(f'SELECT GetUserIdByNickname("{nickname}")')
+    def get_user_by_nickname(cls, nickname: str):
+        '''
+        Get discord user info by nickname
+
+        Return values: 
+
+            Success:
+
+                dict: {'id', 'inviter_id', 'status'}
+
+            User not exist:
+
+                None
+        '''
+        result = cls.execute_query(f'CALL GetUserByNickname("{nickname}")')
+        if (result == -1):
+            return None
+        else:
+            user = dict()
+            user['id'] = result[0][0]
+            user['inviter_id'] = result[0][1]
+            user['status'] = result[0][2]
+            return user
+
+    @classmethod
+    def get_players_by_id(cls, user_id: int):
+        '''
+        Get players by discord user id (get mc accounts of user)
+
+        Return values: 
+
+            Success:
+
+                dict: {'username': {'realname', 'ip', 'lastlogin', 'regdate', 'isLogged'}}
+
+            User not exist:
+
+                None
+        '''
+        result = cls.execute_query(f'CALL GetPlayersByUserID({user_id})')
+        if (result == -1):
+            return None
+        else:
+            players = dict()
+            for player_info in result:
+                players[player_info[0]] = {'realname': player_info[1], 'ip': player_info[2], 'lastlogin': player_info[3], 'regdate': player_info[4], 'isLogged': player_info[5]}
+            return players
+
+    def set_status_by_user_id(cls, user_id: int, status: str):
+        '''
+        Set player's status
+
+        Return values: 
+
+            Success: 1
+
+            Failed: 0
+        '''
+        result = cls.execute_query(f'UPDATE discord SET status = "{status}" WHERE id = {user_id} LIMIT 1')
         if (result == -1):
             return 0
         else:
-            if (result[0][0] == None):
-                return 0
-            return result[0][0]
-
-# def authmeSHA256(password: str, salt: str = None):
-#     if (salt == None):
-#         salt = secrets.token_hex(8)
-#     hash = hashlib.sha256(password.encode('utf-8')).hexdigest()
-#     hash += salt
-#     hash = hashlib.sha256(hash.encode('utf-8')).hexdigest()
-#     return f'$SHA${salt}${hash}'
+            return 1
 
 db = Database()
