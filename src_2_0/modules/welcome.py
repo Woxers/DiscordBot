c6d4b852723e411e8137cdd4714bbf4267cf18d9
Woxerss <maleev2002@yandex.ru>
2022-07-15 18:20:30 +0300

    Пример работы embed из json

--- src_2_0/modules/welcome.py
+++ src_2_0/modules/welcome.py
@@ -1,9 +1,11 @@
 import discord
 
+from discord.ext import commands
+
 from logger import log_error, log_info
 from config import get_color, config
-#from libs import Database
-from discord.ext import commands
+
+from database import Database
 
 class WelcomeCog(commands.Cog):
     def __init__(self, bot):
@@ -11,7 +13,18 @@ class WelcomeCog(commands.Cog):
 
     @commands.Cog.listener()
     async def on_ready(self):
-        log_info("Welcome Module succesfully loaded")
+        log_info("Welcome Module successfully loaded")
+
+    @commands.command(name='test')
+    @commands.has_permissions(administrator = True)
+    async def test(self, ctx, arg = None):
+        dt = dict()
+        dt['USER_NAME'] = 'Aptem'
+        dt['USER_MENTION'] = '<@222746438814138368>'
+        dt['URL'] = 'https://ictis.alex-b.me/'
+        dt['COLOR'] = get_color('neutral')
+        dt['TIMESTAMP'] = '2022-07-15 13:04:06'
+        await self.bot.send_json_embed(ctx, 'example/key.txt', replace_dict=dt, delete_after = 10)
 
 async def setup(bot):
     await bot.add_cog(WelcomeCog(bot))
